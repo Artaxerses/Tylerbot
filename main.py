8a535e5af74e7c0f4e9f5f466d348fa7ad5a376f
Artaxerses <31361963+Artaxerses@users.noreply.github.com>
2017-12-26 20:55:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,30 +9,53 @@ bot = commands.Bot(command_prefix = '$', description = 'Hopefully this works!')
 # Bot event notifies users when it logs on
 @bot.event
 async def on_ready():
-	print('Tylerbot has awakened')
+        await bot.change_presence(game=discord.Game(name='Diplomacy'))
+        await bot.send_message(bot.get_channel('336262010033405952'), 'Tylerbot has awakened')
 
 @bot.event
 async def on_reaction_add(reaction, user):
-    quote = reaction.message
-    if reaction.emoji.name == 'Tyler' and reaction.count == 10:
-        with open("quotes.txt", 'a') as quotefile:
-            quotefile.writelines('\n'+quote.content)
-        await bot.send_message(reaction.message.channel,'Quote "'+reaction.message.content+'" recorded!')
-
+        if reaction.emoji == '\N{THUMBS UP SIGN}' and reaction.count == 8:
+                message = reaction.message.content.split('"')[1]
+                with open("quotes.txt", 'a') as quotefile:
+                        quotefile.writelines('\n'+message)
+                await bot.send_message(reaction.message.channel,'Quote "'+message+'" recorded!')
+                await bot.delete_message(reaction.message)
+        try:
+                if reaction.emoji.name == 'Tyler' and reaction.count == 1:
+                        polling = bot.get_channel('344566152535474186')
+                        general = bot.get_channel('335560493454327808')
+                        bot_chat = bot.get_channel('336262010033405952')
+                        await bot.send_message(general, 'Voting for "'+reaction.message.content+'" has begun in '+polling.mention)
+                        message = await bot.send_message(polling, 'Emoji this to add it to quotes (we need 8): "'+reaction.message.content+'"')
+                        await bot.add_reaction(message, '\N{THUMBS UP SIGN}')
+        except:
+                pass
 @bot.command()
 async def hello():
-	await bot.say("Hey")
+    await bot.say("Hey")
+
+@bot.command()
+async def tylerdanh(arg):
+    quotes = []
+    with open("quotes.txt", 'r') as quotefile:
+        for line in quotefile:
+            quotes.append(line.strip())
+    await bot.say(quotes[arg])
 
 @bot.command()
 async def tyler():
     quotes = []
-    index = 0
     with open("quotes.txt", 'r') as quotefile:
         for line in quotefile:
             quotes.append(line.strip())
-        if (index == len(quotes)):
-        	index = 0
-    await bot.say(quotes[index])
-    index += 1
+    await bot.say(quotes[randint(0, len(quotes)-1)])
 
-bot.run('//insert key here)
+@bot.command(pass_context = True)
+async def shutdown(ctx):
+    if ctx.message.author.id == '196866248984887296':
+        await bot.send_message(bot.get_channel('336262010033405952'), 'i gotta throw my croissants in the oven')
+        await bot.close()
+    else:
+        await bot.say('That sounds like a threat!')
+
+bot.run('MzUxMTgxMjY4NjcyNzA4NjIw.DIPdzA.S2CoSYekiQEUXw7rzywufihhyR4')
